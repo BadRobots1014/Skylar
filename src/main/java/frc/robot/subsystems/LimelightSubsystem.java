46c4e0a1548d3d1455354d906e935eb35241823b
TheMagneticDude <110270128+TheMagneticDude@users.noreply.github.com>
2024-01-29 19:53:17 -0500

    Working April Tag Detection
    Obliberated april tag detection
    Can detect x and y coordinate
    ta area in limelight
    and can detect april tag ID value.

--- src/main/java/frc/robot/subsystems/LimelightSubsystem.java
+++ src/main/java/frc/robot/subsystems/LimelightSubsystem.java
@@ -1,47 +1,67 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
 package frc.robot.subsystems;
-
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
-import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-
-import java.util.function.DoubleSupplier;
 
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class LimelightSubsystem {
-    private ShuffleboardTab m_limelightTab = Shuffleboard.getTab("Limelight");;
+public class LimelightSubsystem extends SubsystemBase {
+  /** Creates a new ExampleSubsystem. */
+  private final ShuffleboardTab m_tab = Shuffleboard.getTab("Limelight");
+  
+  public LimelightSubsystem() {
 
     NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
-    NetworkTableEntry tx = table.getEntry("tx");
-    NetworkTableEntry ty = table.getEntry("ty");
-    NetworkTableEntry ta = table.getEntry("ta");
+    table.getEntry("pipeline").setNumber(2);
+    // NetworkTableEntry tx = table.getEntry("tx");
+    // NetworkTableEntry ty = table.getEntry("ty"); 
+    // NetworkTableEntry ta = table.getEntry("ta");
+    System.out.println("LIMELIGHT SUBSYSTEM");
+    m_tab.addNumber("AprilTag ID: ", this::getAprilTagID);
+    m_tab.addNumber("Tag tx", this::getTx);
+    m_tab.addNumber("Tag ty", this::getTy);
+    m_tab.addNumber("Tag ta", this::getTa);
+  
+  }
 
-    DoubleSupplier LimelightX = new DoubleSupplier() {
-            public double getAsDouble()
-            {
-                return tx.getDouble(0.0);
-            }};
-    DoubleSupplier LimelightY = new DoubleSupplier() {
-            public double getAsDouble()
-            {
-                return ty.getDouble(0.0);
-            }};
-    DoubleSupplier LimelightA = new DoubleSupplier() {
-            public double getAsDouble()
-            {
-                return ta.getDouble(0.0);
-            }};
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
 
-    //read values periodically
+  }
 
-    public LimelightSubsystem()
-    {
-        m_limelightTab.addDouble("LimelightX", LimelightX);
-        m_limelightTab.addDouble("LimelightY", LimelightY);
-        m_limelightTab.addDouble("LimelightA", LimelightA);
-    }
+  public double getAprilTagID(){
+    NetworkTableEntry tID = NetworkTableInstance.getDefault().getTable("limelight").getEntry("tid");
+    double AprilTagID = tID.getDouble(0.0);
+    return AprilTagID;
+  }
+
+  public double getTx(){
+    double tx = NetworkTableInstance.getDefault().getTable("limelight").getEntry("tx").getDouble(0.0);
+    return tx;
+  }
+
+  public double getTy(){
+    double ty = NetworkTableInstance.getDefault().getTable("limelight").getEntry("ty").getDouble(0.0);
+    return ty;
+  }
+
+  public double getTa(){
+    double ta = NetworkTableInstance.getDefault().getTable("limelight").getEntry("ta").getDouble(0.0);
+    return ta;
+  }
+  
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+
+  
 }
-
-// Testing...
